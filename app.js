50d2c275423a143e51748a5ad368bcd6baab6099
Ayushi <aprakash162001@gmail.com>
2020-10-24 19:34:29 +0530

    Responsive Design

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const options = {
-  bottom: "675px", // default: '32px'
-  right: "unset", // default: '32px'
-  left: "32px", // default: 'unset'
-  time: "0.3s", // default: '0.3s'
-  mixColor: "#fff", // default: '#fff'
-  backgroundColor: "#fff", // default: '#fff'
-  buttonColorDark: "#100f2c", // default: '#100f2c'
-  buttonColorLight: "#fff", // default: '#fff'
+  bottom: '210pxpx', // default: '32px'
+  right: '16px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.3s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: true, // default: true,
-  label: "🌓", // default: ''
-  autoMatchOsTheme: true, // default: true
-};
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
+
 
 function getHistory() {
   return document.getElementById("history-value").innerText;
